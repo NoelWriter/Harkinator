7123753e875deb50b1f6d16465e2f23bfea7c2b3
Gijs <gijsvdabeele@gmail.com>
2021-03-16 14:23:08 +0100

    Added headless chrome driver and config toggle

--- src/client/webdriver.js
+++ src/client/webdriver.js
@@ -1,12 +1,27 @@
 const webdriver = require("selenium-webdriver");
+const chrome = require('selenium-webdriver/chrome');
+const config = require("../../config.json");
+//const phantomjs = require("phantomjs")npm 
 
 module.exports = {
     start(URL = "https://capital.com/trading/platform/") {
-        const driver = new webdriver.Builder()
-            .forBrowser('firefox')
+        if (config.HEADLESS) {
+            const driver = new webdriver.Builder()
+            .forBrowser('chrome')
+            .setChromeOptions(new chrome.Options().addArguments("--lang=nl").headless())
             .build()
         driver.get(URL)
         return driver
+        }
+        else{
+            const driver = new webdriver.Builder()
+            .forBrowser('chrome')
+            .setChromeOptions(new chrome.Options().addArguments("--lang=nl"))
+            .build()
+        driver.get(URL)
+        return driver
+        }
+
     }
 }
 
