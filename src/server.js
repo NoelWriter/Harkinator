a5dc283e8e295f75509fe31bb124a2234f134c65
Nathanael Baaij <nathanaelbaaij@gmail.com>
2021-03-21 19:33:57 +0100

    Added fields

--- src/server.js
+++ src/server.js
@@ -20,6 +20,93 @@ app.get("/config", (req, res) => {
 
 // post config
 app.post("/config", function (req, res) {
+  if (req.body.hasOwnProperty('STOCK_FRACTION_DIGITS')) {
+    req.body.STOCK_FRACTION_DIGITS = parseInt(req.body.STOCK_FRACTION_DIGITS);
+  }
+
+  if (req.body.hasOwnProperty('STOCK_MULTIPLIER_ABOVE_SELL')) {
+    req.body.STOCK_MULTIPLIER_ABOVE_SELL = parseFloat(req.body.STOCK_MULTIPLIER_ABOVE_SELL);
+  }
+
+  if (req.body.hasOwnProperty('STOCK_AMOUNT')) {
+    req.body.STOCK_AMOUNT = parseFloat(req.body.STOCK_AMOUNT);
+  }
+
+  if (req.body.hasOwnProperty('STOCK_PROFIT')) {
+    req.body.STOCK_PROFIT = parseFloat(req.body.STOCK_PROFIT);
+  }
+
+  if (req.body.hasOwnProperty('STOCK_PROBE_AMOUNT')) {
+    req.body.STOCK_PROBE_AMOUNT = parseFloat(req.body.STOCK_PROBE_AMOUNT);
+  }
+
+  if (req.body.hasOwnProperty('NUM_INSTANCES')) {
+    req.body.NUM_INSTANCES = parseInt(req.body.NUM_INSTANCES);
+  }
+
+  if (req.body.hasOwnProperty('STOCK_BUY_UPPER_LIMIT')) {
+    req.body.STOCK_BUY_UPPER_LIMIT = parseFloat(req.body.STOCK_BUY_UPPER_LIMIT);
+  }
+
+  if (req.body.hasOwnProperty('STOCK_BUY_LOWER_LIMIT')) {
+    req.body.STOCK_BUY_LOWER_LIMIT = parseFloat(req.body.STOCK_BUY_LOWER_LIMIT);
+  }
+
+  if (req.body.hasOwnProperty('STOCK_BUY_FILL_WAIT')) {
+    req.body.STOCK_BUY_FILL_WAIT = parseInt(req.body.STOCK_BUY_FILL_WAIT);
+  }
+
+  if (req.body.hasOwnProperty('STOCK_SELL_UPPER_LIMIT')) {
+    req.body.STOCK_SELL_UPPER_LIMIT = parseFloat(req.body.STOCK_SELL_UPPER_LIMIT);
+  }
+
+  if (req.body.hasOwnProperty('STOCK_SELL_LOWER_LIMIT_BREAK_EVEN')) {
+    req.body.STOCK_SELL_LOWER_LIMIT_BREAK_EVEN = parseFloat(req.body.STOCK_SELL_LOWER_LIMIT_BREAK_EVEN);
+  }
+
+  if (req.body.hasOwnProperty('STOCK_SELL_LOWER_LIMIT_LOSS')) {
+    req.body.STOCK_SELL_LOWER_LIMIT_LOSS = parseFloat(req.body.STOCK_SELL_LOWER_LIMIT_LOSS);
+  }
+
+  if (req.body.hasOwnProperty('STOCK_EVALUATE_DELTA_UP')) {
+    req.body.STOCK_EVALUATE_DELTA_UP = parseFloat(req.body.STOCK_EVALUATE_DELTA_UP);
+  }
+
+  if (req.body.hasOwnProperty('STOCK_EVALUATE_DELTA_DOWN')) {
+    req.body.STOCK_EVALUATE_DELTA_DOWN = parseFloat(req.body.STOCK_EVALUATE_DELTA_DOWN);
+  }
+
+  if (req.body.hasOwnProperty('STOCK_LOSS_MULTIPLIER')) {
+    req.body.STOCK_LOSS_MULTIPLIER = parseFloat(req.body.STOCK_LOSS_MULTIPLIER);
+  }
+
+  if (req.body.hasOwnProperty('FREEFALL_INDICATOR')) {
+    req.body.FREEFALL_INDICATOR = parseFloat(req.body.FREEFALL_INDICATOR);
+  }
+
+  if (req.body.hasOwnProperty('STOCK_AVERAGE_ITERATIONS')) {
+    req.body.STOCK_AVERAGE_ITERATIONS = parseInt(req.body.STOCK_AVERAGE_ITERATIONS);
+  }
+
+  if (req.body.hasOwnProperty('STOCK_AVERAGE_TIME_DELAY')) {
+    req.body.STOCK_AVERAGE_TIME_DELAY = parseInt(req.body.STOCK_AVERAGE_TIME_DELAY);
+  }
+
+  if (req.body.hasOwnProperty('DELAY_PLATFORM_LAG')) {
+    req.body.DELAY_PLATFORM_LAG = parseInt(req.body.DELAY_PLATFORM_LAG);
+  }
+
+  if (req.body.hasOwnProperty('DELAY_FREEFALL_DETECTED')) {
+    req.body.DELAY_FREEFALL_DETECTED = parseInt(req.body.DELAY_FREEFALL_DETECTED);
+  }
+
+  if (req.body.hasOwnProperty('LAG_MAX_ORDER_DELAY')) {
+    req.body.LAG_MAX_ORDER_DELAY = parseInt(req.body.LAG_MAX_ORDER_DELAY);
+  }
+
+  if (req.body.hasOwnProperty('LIVE_ACCOUNT_NUM')) {
+    req.body.LIVE_ACCOUNT_NUM = parseInt(req.body.LIVE_ACCOUNT_NUM);
+  }
 
   if (req.body.hasOwnProperty('STOCK_ROUND_TO_WHOLE')) {
     req.body.STOCK_ROUND_TO_WHOLE = req.body.STOCK_ROUND_TO_WHOLE === 'true' ? true : false; 
