c2735bdc67c881e17d3773b65e207021d3f8c8ce
gijsvdabeele <gijsvdabeele@gmail.com>
2021-03-21 12:27:20 +0100

    Fixed memory issue

--- src/client/client.js
+++ src/client/client.js
@@ -15,6 +15,11 @@ const {By} = require("selenium-webdriver");
 
 module.exports = {
     instanceName: "",
+    stockName: "",
+    intance: 0,
+    discordClientInstance: "",
+    driverStartDate: Date.now(),
+
     /**
      * Execute client instance
      *
@@ -25,12 +30,19 @@ module.exports = {
      */
     async execute (stockName, instance, delay, discordClientInstance) {
 
+        this.stockName = stockName
+        this.instance = instance
+        this.discordClientInstance = discordClientInstance
+        this.driverStartDate = Date.now()
+
         // Timeout between starting instances to prevent internet clogging
         await new Promise(resolve => setTimeout(resolve, delay));
 
         // Initialize webdriver
         utils.log.generic(`Initialising Harkinator: ` + instance)
-        const driver = await webdriver.start()
+        
+        let driver = await getDriver()
+        
 
         discordClientInstance.on("message", msg => {
             if (msg.content.toLowerCase() === 's' && msg.author.id === config.DISCORD_USERID) {
@@ -50,9 +62,100 @@ module.exports = {
 
         while (true) {
             await utils.checkPause(driver)
-            await trade(driver, stockElement)
+            await this.trade(driver, stockElement)
         }
     },
+
+    /**
+     * @param {*} driver
+     * @param {*} stockElement
+     */
+    async trade(driver, stockElement) {
+        utils.log.generic(`Starting trade`)
+
+        if ((Date.now() - this.driverStartDate) > 7200000) {
+            driver.quit()
+            await this.execute(this.stockName, this.instance, 0, this.discordClientInstance)
+        }
+
+        // Clear all open orders
+        await utils.clearOpenOrders(driver)
+
+        // Close trading panel to prevent buying high
+        try {
+            await driver.findElement(By.xpath(locations.order_panel_close_button)).click()
+        } catch (e) { }
+
+        // Check if pause has been called
+        await utils.checkPause(driver)
+
+        // Clear any open positions
+        if (await utils.getPositionsTotal(driver) !== 0) {
+            utils.log.error("Positions are still open!")
+            
+            if (config.FORCE_CLOSE_OPEN_POSITIONS)
+                await utils.clearOpenPosition(driver)
+                
+        }
+
+        // Fetch current spread
+        const initialSpread = await utils.getSpread(stockElement)
+        utils.log.generic(`Initial spread: ${initialSpread}`)
+
+        // Probe lag to maintain bot functionality
+        utils.log.generic(`Probing platform lag...`)
+        const platformLag = await probePlatformLatency(driver, stockElement)
+        utils.log.generic(`Buy order delay is currently ${platformLag}ms`)
+
+        if (platformLag > config.LAG_MAX_ORDER_DELAY) {
+            const sleepAmount = config.DELAY_PLATFORM_LAG
+            utils.log.warning(`Platform lag detected, buy order delay is currently ${platformLag}ms. Hibernating for ${sleepAmount/1000} seconds`)
+            await driver.sleep(sleepAmount)
+            return
+        }
+
+        // Find buy price
+        const price = await findPrice.buy(driver, stockElement, config.STOCK_MULTIPLIER_ABOVE_SELL)
+        const curSellLevel = await utils.getStockSellPrice(stockElement)
+        utils.log.generic(`Found price at ${price}`)
+
+        // Create buy order
+        const sellLevel = await createBuyOrder.execute(driver, stockElement, config.STOCK_AMOUNT, price, curSellLevel)
+        if (!sellLevel)
+            return
+
+        // Wait for buy order to be filled
+        const boughtSellLevel = await awaitBuyOrder.execute(driver, stockElement, config.STOCK_AMOUNT, sellLevel)
+        if (!boughtSellLevel)
+            return
+
+        // Find the sell price for the positions held
+        let curSellPrice = await findPrice.sell(driver, stockElement, config.STOCK_PROFIT)
+        utils.log.debug("Sell price : " + curSellPrice.toString())
+
+        // Loop to keep updating sell position
+        while (await utils.getPositionsTotal(driver) > 0) {
+            // Clear open orders
+            await utils.clearOpenOrders(driver)
+            await utils.checkPause(driver)
+
+            // Get amount of positions to create orders for
+            let positions = await utils.getPositionsTotal(driver)
+
+            // Create an order for current position
+            const curSellPriceLevel = await createSellOrder.execute(driver, stockElement, positions, curSellPrice)
+            if (!curSellPriceLevel)
+                continue
+
+            // Check for changes in price or fulfillment
+            const newSellPrice = await awaitSellOrder.execute(driver, stockElement, positions, boughtSellLevel, curSellPrice, curSellPriceLevel)
+            utils.log.debug("New sell price : " + newSellPrice.toString())
+            if (newSellPrice)
+                curSellPrice = newSellPrice
+        }
+
+        utils.log.discord(`:moneybag: ${await utils.getBalance(driver)} :moneybag:`)
+    }
 }
 
 /**
@@ -75,97 +178,15 @@ async function init(driver, instance) {
     return stockElement
 }
 
-/**
- * @param {*} driver
- * @param {*} stockElement
- */
-async function trade(driver, stockElement) {
-    utils.log.generic(`Starting trade`)
-
-    // Clear all open orders
-    await utils.clearOpenOrders(driver)
-
-    // Close trading panel to prevent buying high
-    try {
-        await driver.findElement(By.xpath(locations.order_panel_close_button)).click()
-    } catch (e) { }
-
-    // Check if pause has been called
-    await utils.checkPause(driver)
-
-    // Clear any open positions
-    if (await utils.getPositionsTotal(driver) !== 0) {
-        utils.log.error("Positions are still open!")
-        
-        if (config.FORCE_CLOSE_OPEN_POSITIONS)
-            await utils.clearOpenPosition(driver)
-            
-    }
-
-    // Fetch current spread
-    const initialSpread = await utils.getSpread(stockElement)
-    utils.log.generic(`Initial spread: ${initialSpread}`)
-
-    // Probe lag to maintain bot functionality
-    utils.log.generic(`Probing platform lag...`)
-    const platformLag = await probePlatformLatency(driver, stockElement)
-    utils.log.generic(`Buy order delay is currently ${platformLag}ms`)
-
-    if (platformLag > config.LAG_MAX_ORDER_DELAY) {
-        const sleepAmount = config.DELAY_PLATFORM_LAG
-        utils.log.warning(`Platform lag detected, buy order delay is currently ${platformLag}ms. Hibernating for ${sleepAmount/1000} seconds`)
-        await driver.sleep(sleepAmount)
-        return
-    }
-
-    // Find buy price
-    const price = await findPrice.buy(driver, stockElement, config.STOCK_MULTIPLIER_ABOVE_SELL)
-    const curSellLevel = await utils.getStockSellPrice(stockElement)
-    utils.log.generic(`Found price at ${price}`)
-
-    // Create buy order
-    const sellLevel = await createBuyOrder.execute(driver, stockElement, config.STOCK_AMOUNT, price, curSellLevel)
-    if (!sellLevel)
-        return
-
-    // Wait for buy order to be filled
-    const boughtSellLevel = await awaitBuyOrder.execute(driver, stockElement, config.STOCK_AMOUNT, sellLevel)
-    if (!boughtSellLevel)
-        return
-
-    // Find the sell price for the positions held
-    let curSellPrice = await findPrice.sell(driver, stockElement, config.STOCK_PROFIT)
-    utils.log.debug("Sell price : " + curSellPrice.toString())
-
-    // Loop to keep updating sell position
-    while (await utils.getPositionsTotal(driver) > 0) {
-        // Clear open orders
-        await utils.clearOpenOrders(driver)
-        await utils.checkPause(driver)
-
-        // Get amount of positions to create orders for
-        let positions = await utils.getPositionsTotal(driver)
-
-        // Create an order for current position
-        const curSellPriceLevel = await createSellOrder.execute(driver, stockElement, positions, curSellPrice)
-        if (!curSellPriceLevel)
-            continue
-
-        // Check for changes in price or fulfillment
-        const newSellPrice = await awaitSellOrder.execute(driver, stockElement, positions, boughtSellLevel, curSellPrice, curSellPriceLevel)
-        utils.log.debug("New sell price : " + newSellPrice.toString())
-        if (newSellPrice)
-            curSellPrice = newSellPrice
-    }
-
-    utils.log.discord(`:moneybag: ${await utils.getBalance(driver)} :moneybag:`)
-}
-
 async function probePlatformLatency(driver, stockElement) {
     let t0 = Date.now()
     await createBuyOrder.execute(driver, stockElement, config.STOCK_PROBE_AMOUNT, await utils.getStockSellPrice(stockElement) * 0.8)
     let t1 = Date.now()
     await utils.clearOpenOrders(driver)
     return t1 - t0
+}
+
+async function getDriver() {
+    return await webdriver.start()
 }
 
