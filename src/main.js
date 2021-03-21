0feca1b656d246bfc817c6d60ab73884c65fce22
Nathanael <nathanaelbaaij@gmail.com>
2021-03-21 09:46:10 +0100

    Update

--- src/main.js
+++ src/main.js
@@ -7,31 +7,27 @@ const utils = require("./utils/utils")
 const discordClient = require("./client/discordClient")
 
 discordClient.init(config.DISCORD_TOKEN).then((discordClientInstance) => {
- 
-
     const instance = parseInt(process.argv[2])
-
     client.execute(config.STOCK_PRIMARY, instance+1, 10000*instance, discordClientInstance)
-
 })
 
 // Error handling
-process.on('SIGINT', () => {
-    process.exit();
-});
+// process.on('SIGINT', () => {
+//     process.exit();
+// });
 
-process.on("unhandledRejection", (error) => {
-    utils.log.error(`Uncaught Error: ${error}`)
-});
+// process.on("unhandledRejection", (error) => {
+//     utils.log.error(`Uncaught Error: ${error}`)
+// });
 
-process.on("TypeError", (error) => {
-    utils.log.error(`Uncaught Error: ${error}`)
-});
+// process.on("TypeError", (error) => {
+//     utils.log.error(`Uncaught Error: ${error}`)
+// });
 
-process.on("uncaughtExceptionMonitor", (error) => {
-    utils.log.error(`Uncaught Exception: ${error}`)
-});
+// process.on("uncaughtExceptionMonitor", (error) => {
+//     utils.log.error(`Uncaught Exception: ${error}`)
+// });
 
-process.on("warning", (warning) => {
-    utils.log.warning(`Warning ${warning}`)
-});
+// process.on("warning", (warning) => {
+//     utils.log.warning(`Warning ${warning}`)
+// });
