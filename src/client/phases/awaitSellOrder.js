634dd918796f08956132942791225885e3cf7cf6
gijsvdabeele <gijsvdabeele@gmail.com>
2021-03-23 21:10:47 +0100

    Added profit mode that actually profits

--- src/client/phases/awaitSellOrder.js
+++ src/client/phases/awaitSellOrder.js
@@ -31,7 +31,7 @@ module.exports = {
                     let sellPrice = await utils.getStockSellPrice(stockElement)
                     return sellPrice += spread * config.STOCK_LOSS_MULTIPLIER
                 case modes.PROFIT:
-                    return await findPrice.sell(driver, stockElement, config.STOCK_PROFIT * 2.5)
+                    return await findPrice.sell(driver, stockElement, curSellPrice + (0.2 * await utils.getSpread()))
                 case modes.BREAK_EVEN:
                     return await findPrice.sell(driver, stockElement, 0)
             }
