7d974bbc3fc2b06bfedf38a0dbf48e32c45c4b42
gijsvdabeele <gijsvdabeele@gmail.com>
2021-03-26 15:56:06 +0100

    Fixed profit mode maybe

--- src/client/phases/awaitSellOrder.js
+++ src/client/phases/awaitSellOrder.js
@@ -32,9 +32,7 @@ module.exports = {
                     return sellPrice += spread * config.getConfigValue('STOCK_LOSS_MULTIPLIER')
                 case modes.PROFIT:
                     utils.log.warning(`Sell order is in profit mode`)
-                    const spread = await utils.getSpread(stockElement)
-                    let sellPrice = await utils.getStockSellPrice(stockElement)
-                    return sellPrice += spread * 0.2
+                    return (await utils.getStockSellPrice(stockElement)) + (await utils.getSpread(stockElement) * 0.2)
                 case modes.BREAK_EVEN:
                     utils.log.warning(`Sell order is in break even mode`)
                     return await findPrice.sell(driver, stockElement, 0)
