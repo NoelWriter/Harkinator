55b595e04f3c7129857567a5bc28b31c32ea121d
Noël <schr0112@hz.nl>
2021-04-18 13:59:20 +0200

    Fixed replace functions to support main version

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -10,7 +10,7 @@ module.exports = {
         try {
             let prices = await element.findElements(By.className("price-ticker-value2"))
             let price = await prices[1].findElement(By.className("price-ticker-number")).getAttribute("innerHTML")
-            return parseFloat(price.replace(',', ''))
+            return parseFloat(price.replace('.', '').replace(',', '.'))
         } catch (e) {
             this.log.warning("Error getting buyprice.")
             return false
@@ -22,7 +22,7 @@ module.exports = {
         try {
             let prices = await element.findElements(By.className("price-ticker-value2"))
             let price = await prices[0].findElement(By.className("price-ticker-number")).getAttribute("innerHTML")
-            return parseFloat(price.replace(',', ''))
+            return parseFloat(price.replace('.', '').replace(',', '.'))
         } catch (e) {
             this.log.warning("Error getting sellprice.")
             return false
@@ -67,7 +67,7 @@ module.exports = {
         for (const stockListElement of stockListElements) {
             try {
                 const positionAmountString = await stockListElement.findElement(By.className("entry")).getText()
-                const positionAmount = parseFloat(positionAmountString.split(" ")[1].replace(',', ''))
+                const positionAmount = parseFloat(positionAmountString.split(" ")[1].replace('.', '').replace(',','.'))
                 if (positionAmount > highestPositionAmount)
                     highestPositionAmount = positionAmount
             } catch (e) {
